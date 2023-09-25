97618929c4d81487b6625b640e3ef84b1d299158
bhxspring <850921703@qq.com>
2023-09-25 15:43:19 +0800

    Update CDOTD.m

--- CDOTD code/CDOTD.m
+++ CDOTD code/CDOTD.m
@@ -4,7 +4,7 @@ function [G,W,obj] = CDOTD(X,St,W1,StartInd,nClusters,lambda)
 % X:         the d*n centered data matrix
 % St:        the d*d total scatter matrix
 % W1:        the d*m initial projection matrix
-% StartInd:  50 randomly initialized cluster vectors
+% StartInd:  one of the 50 randomly initialized cluster vectors
 % nClusters: the number of clusters in the data set
 % lambda:    the balance parameter
 % The code is written by Quan Wang 
@@ -16,6 +16,7 @@ m = size(W1,2);
 W = W1;
 G = full(ind2vec(StartInd',nClusters))';
 
+obj = zeros(maxIter,1);
 it = 0;
 obj_old = 0;
 obj_new = 0.1;
@@ -24,7 +25,7 @@ while abs((obj_old-obj_new)/obj_new)>epsilon&&it<maxIter
     obj_old = obj_new;    
     
     U = W'*X;
-    [G,~] = CDO(U,nClusters,G);  
+    [G,~] = CDO(U,G);  
     
     Sb = X*G*(G'*G)^-1*G'*X';
     M = Sb-lambda*St;
@@ -34,80 +35,42 @@ while abs((obj_old-obj_new)/obj_new)>epsilon&&it<maxIter
     obj(it) = obj_new;
 end
 if it == maxIter
-    disp(['Warnning: the CDOTD does not converge within ',num2str(maxIter),' iterations!']);
+    disp('Warnning: the CDOTD does not converge within maxIter iterations!');
 end
-dbstop if error
+if it<maxIter
+    obj(it+1:end) = [];
+end
+% dbstop if error
 
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% subfunction %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [G,ob] = CDO(U,nClusters,G)
-% solve G = arg max trace( (G'*G)^(-1)*(G'*U'*U*G) ) 
-% ob:        the objective function value
-% U:         the m*n matrix
-% nClusters: the number of clusters in the data set
-% G:         the n*c cluster indicator matrix
-% The code is written by Quan Wang
+function [G,ob] = CDO(U,G)
 
-epsilon = 1e-8;
-N = zeros(nClusters,1);
-D = zeros(nClusters,1);
-N0 = zeros(nClusters,1);
-D0 = zeros(nClusters,1);
-Nk = zeros(nClusters,1);
-Dk = zeros(nClusters,1);
 T = U*G;
-for i=1:nClusters
-    N(i) = T(:,i)'*T(:,i);
-    D(i) = G(:,i)'*G(:,i);
-end
+N = sum(T.*T,1)';
+D = sum(G.*G,1)';
+Uii = sum(U.*U,1)';
+
+maxIter = 100;
+epsilon = 1e-8;
+obj = zeros(maxIter+2,1);
 iter = 2;
-obj(1) = 1;
 obj(2) = sum(N./D);
-numPts = size(U,2);
-Uii = zeros(numPts,1);
-for i=1:numPts
-    Uii(i) = U(:,i)'*U(:,i);
-end
 while abs((obj(iter-1)-obj(iter))/obj(iter))>epsilon
-    iter = iter+1;    
-    for i=1:numPts
-        p = find(G(i,:)==1);        
-        if D(p)>1
-            G(i,p) = 0;
-        else
-            continue;
-        end        
-        for k=1:nClusters
-            if k==p
-                Nk(k) = N(k); 
-                Dk(k) = D(k);
-                N0(k) = N(k)-2*U(:,i)'*T(:,k)+Uii(i); 
-                D0(k) = D(k)-1;
-            else
-                N0(k) = N(k); 
-                D0(k) = D(k);
-                Nk(k) = N(k)+2*U(:,i)'*T(:,k)+Uii(i); 
-                Dk(k) = D(k)+1;
-            end
-        end
-        delta = Nk./Dk-N0./D0;
-        [~,q] = max(delta);
-        G(i,q) = 1;
-        if q~=p
-            T(:,p) = T(:,p)-U(:,i);
-            T(:,q) = T(:,q)+U(:,i);
-            N(p) = N0(p); 
-            D(p) = D0(p);
-            N(q) = Nk(q); 
-            D(q) = Dk(q);
-        end
+    if iter>maxIter
+        disp('Warnning: The maxIter iterations has reached in CDO!');
+        break;
     end
+    iter = iter+1;
+
+    [G,N,D,T] = updateArrayG(G,N,D,U,T,Uii);
+
     obj(iter) = sum(N./D);
 end
-iter = iter-2;
-obj(1) = [];
-ob = obj(end);
+ob = obj(iter);
+% iter = iter-2;
+
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% subfunction %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
